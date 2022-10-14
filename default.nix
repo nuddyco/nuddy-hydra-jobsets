7d5db6df1cafa0d18bf6c4a37ba5582d21bf14f5
Luke Gorrie <luke@nuddy.net>
2022-10-14 12:49:39 +0200

    Initial commit based on shlevy example

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    {
+        "master": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "js",
+            "nixexprinput": "src",
+            "nixexprpath": "release.nix",
+            "checkinterval": 300,
+            "schedulingshares": 100,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 3,
+            "inputs": {
+                "src": { "type": "git", "value": "git://github.com/shlevy/declarative-hydra-example.git", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs.git release-16.03", "emailresponsible": false }
+            }
+        }
+    }
+
+    EOF
+  '';
+}
