f6075e939af1a01d0ae08570717ab8eb7b869bb3
Luke Gorrie <luke@nuddy.net>
2022-10-18 13:24:14 +0200

    default.nix: Refactor JSON jobset definition

--- default.nix
+++ default.nix
@@ -1,28 +1,39 @@
-{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+{ nixpkgs ? <nixpkgs>
+, declInput ? {}
+}:
+let pkgs = import nixpkgs {};
+    defaults = { enabled = 1;
+                 hidden = false;
+                 checkinterval = 300;
+                 schedulingshares = 10;
+                 enableemail = false;
+                 emailoverride = "";
+                 keepnr = 100;
+               };
+in
+{
   jobsets = pkgs.runCommand "spec.json" {} ''
     cat <<EOF
     ${builtins.toXML declInput}
     EOF
     cat > $out <<EOF
-    {
-        "master": {
-            "enabled": 1,
-            "hidden": false,
-            "description": "js",
-            "nixexprinput": "src",
-            "nixexprpath": "release.nix",
-            "checkinterval": 300,
-            "schedulingshares": 100,
-            "enableemail": false,
-            "emailoverride": "",
-            "keepnr": 3,
-            "inputs": {
-                "src": { "type": "git", "value": "https://github.com/nuddyco/nuddy-hydra-jobsets", "emailresponsible": false },
-                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs release-22.05", "emailresponsible": false }
-            }
-        }
-    }
-
+    ${builtins.toJSON
+      ({ description = "kons-9 master";
+         nixexprinput = "src";
+         nixexprpath = "release.nix";
+         inputs = {
+           src = {
+             type = "git";
+             value = "https://github.com/nuddyco/nuddy-hydra-jobsets";
+             emailresponsible = false;
+           };
+           nixpkgs = {
+             type = "git";
+             value = "https://github.com/NixOS/nixpkgs release-22.05";
+             emailresponsible = false;
+           };
+         };
+       } // defaults)}
     EOF
   '';
 }
