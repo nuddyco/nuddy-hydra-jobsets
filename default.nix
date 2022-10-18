eedb6a78e67d2dc932858af4ae3e2cad4fc0a792
Luke Gorrie <luke@nuddy.net>
2022-10-18 13:50:00 +0200

    default.nix: Setup initial kons-9 test

--- default.nix
+++ default.nix
@@ -18,22 +18,20 @@ in
     EOF
     cat > $out <<EOF
     ${builtins.toJSON
-      ({ description = "kons-9 master";
-         nixexprinput = "src";
-         nixexprpath = "release.nix";
-         inputs = {
-           src = {
-             type = "git";
-             value = "https://github.com/nuddyco/nuddy-hydra-jobsets";
-             emailresponsible = false;
-           };
-           nixpkgs = {
-             type = "git";
-             value = "https://github.com/NixOS/nixpkgs release-22.05";
-             emailresponsible = false;
-           };
-         };
-       } // defaults)}
+      (let githubrepo = id: { type = "git";
+                              value = "https://github.com/${id}";
+                              emailresponsible = false; };
+       in
+         ({ description = "kons-9 master";
+            nixexprinput = "kons-9";
+            nixexprpath = "kons-9.nix";
+            inputs = {
+              src = githubrepo "nuddyco/nuddy-hydra-jobsets";
+              nixpkgs = githubrepo "NixOS/nixpkgs release-22.05";
+              kons-9 = githubrepo "kaveh808/kons-9";
+              lispnix = githubrepo "nuddyco/lispnix";
+            };
+          } // defaults))}
     EOF
   '';
 }
